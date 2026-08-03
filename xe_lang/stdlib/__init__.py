ba2385c0dc2af324b1fb06b6be71fee20d0e0714
creeperdoesredstone <142895378+creeperdoesredstone@users.noreply.github.com>
2026-08-03 18:03:57 +0700

    Create __init__.py

---
+++ xe_lang/stdlib/__init__.py
@@ -0,0 +1,25 @@
+from xe_lang.stdlib.ids import BuiltInID
+from xe_lang.stdlib.specs import (
+	BUILTIN_SYSCALLS,
+	METHOD_SYSCALLS,
+	PROPERTY_GETTER_SYSCALLS,
+	PROPERTY_SETTER_SYSCALLS,
+	STANDARD_LIBRARY_SPECS,
+	BuiltinSpec,
+	ConstantSpec,
+	LibrarySpec,
+	PropertySpec,
+)
+
+__all__ = [
+	"BUILTIN_SYSCALLS",
+	"METHOD_SYSCALLS",
+	"PROPERTY_GETTER_SYSCALLS",
+	"PROPERTY_SETTER_SYSCALLS",
+	"STANDARD_LIBRARY_SPECS",
+	"BuiltInID",
+	"BuiltinSpec",
+	"ConstantSpec",
+	"LibrarySpec",
+	"PropertySpec",
+]
